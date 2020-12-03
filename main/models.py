6c5bbe2f1f594eda52fb259278948220c6805ad6
martinsmandina <martinsmandina@gmail.com>
2020-12-03 12:34:11 +0300

    add class Post

--- main/models.py
+++ main/models.py
@@ -1,5 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 import datetime as dt
 
 
 # Create your models here.
+class Post(models.Model):
+    username = models.CharField(max_length=50)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    neighbourhood_id = models.ForeignKey(Neighbourhood, null=True,blank=True)
+    posted_at = models.DateTimeField(auto_now_add=True)
