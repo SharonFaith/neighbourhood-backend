71e9f53ad1f1b145af537cf55271da313a866bc1
martinsmandina <martinsmandina@gmail.com>
2020-12-03 13:18:14 +0300

    rectified models fields removed Id

--- main/models.py
+++ main/models.py
@@ -5,23 +5,25 @@ import datetime as dt
 
 
 # Create your models here.
+class Neighbourhood(models.Model):
+
+
 class Post(models.Model):
-    username = models.CharField(max_length=255)
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
-    neighbourhood_id = models.ForeignKey(Neighbourhood, null=True,blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    neighbourhood = models.ForeignKey(Neighbourhood,on_delete=models.CASCADE)
     posted_at = models.DateTimeField(auto_now_add=True)
 
 class Service(models.Model):
     name = models.CharField(max_length=255)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    neighbourhood_id = models.ForeignKey(Neighbourhood, null=True,blank=True)
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    neighbourhood = models.ForeignKey(Neighbourhood, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     email = models.EmailField(max_length=255)
     description = models.TextField(null=True)
 
 class Comment(models.Model):
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
-    post_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
     posted_at = models.DateTimeField(auto_now_add=True)
     content = models.TextField(null=True)
 
