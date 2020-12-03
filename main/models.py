82d6b3efbb7a6218dac286cd64504e5920e52a40
martinsmandina <martinsmandina@gmail.com>
2020-12-03 12:45:56 +0300

    add class Service

--- main/models.py
+++ main/models.py
@@ -6,7 +6,20 @@ import datetime as dt
 
 # Create your models here.
 class Post(models.Model):
-    username = models.CharField(max_length=50)
+    username = models.CharField(max_length=255)
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
     neighbourhood_id = models.ForeignKey(Neighbourhood, null=True,blank=True)
     posted_at = models.DateTimeField(auto_now_add=True)
+
+class Service(models.Model):
+    name = models.CharField(max_length=255)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    neighbourhood_id = models.ForeignKey(Neighbourhood, null=True,blank=True)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    email = models.EmailField(max_length=255)
+    description = models.TextField(null=True)
+
+
+
+
+
