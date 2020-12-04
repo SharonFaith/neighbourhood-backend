4a8bc83188e66a53e6a9f904df248f39b311e665
SharonFaith <sharonfaith15@gmail.com>
2020-12-04 09:07:46 +0300

    add serializer

--- main/serializer.py
+++ main/serializer.py
@@ -5,3 +5,16 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = '__all__'
+
+
+class SignUpSerializer(serializers.ModelSerializer):
+    confirm_password = serializers.CharField(style={'input_type':'password'}, write_only=True)
+
+    class Meta:
+        model = User
+        fields = ['email', 'username', 'first_name', 'last_name', 'password', 'confirm_password']
+        extra_kwargs = {
+            'password':{'write_only':True}
+        }
+
+   # def save(self)
