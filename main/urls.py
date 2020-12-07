86acbe354bf3e88101b7be77d966551c4f5ff10d
SharonFaith <sharonfaith15@gmail.com>
2020-12-07 19:40:54 +0300

    test moving urls

--- main/urls.py
+++ main/urls.py
@@ -3,10 +3,31 @@ from django.conf import settings
 from django.conf.urls.static import static
 from . import views
 from .views import welcome
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Hoods API",
+      default_version='v1',
+      description="API for neighbourhood website",
+      #terms_of_service="https://www.google.com/policies/terms/",
+      #contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=[permissions.AllowAny],
+)
+
+
 
 
 urlpatterns = [
-   
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     #path('', welcome, name='welcome'),
     path('api/users/', views.UserList.as_view(),name='user-list'),
     path('activate-account/<uid>/<token>/', views.activate_account, name='activation_email'),
--- neighbourhood/urls.py
+++ neighbourhood/urls.py
@@ -20,32 +20,39 @@ from django.conf.urls import include, url
 from rest_framework.authtoken.views import obtain_auth_token
 from django.urls import path
 from rest_framework_simplejwt import views as jwt_views
-from rest_framework import permissions
-from drf_yasg.views import get_schema_view
-from drf_yasg import openapi
+# from rest_framework import permissions
+# from drf_yasg.views import get_schema_view
+# from drf_yasg import openapi
 
 
-schema_view = get_schema_view(
-   openapi.Info(
-      title="Hoods API",
-      default_version='v1',
-      description="API for neighbourhood website",
-      #terms_of_service="https://www.google.com/policies/terms/",
-      #contact=openapi.Contact(email="contact@snippets.local"),
-      license=openapi.License(name="BSD License"),
-   ),
-   public=True,
-   permission_classes=[permissions.AllowAny],
-)
+# schema_view = get_schema_view(
+#    openapi.Info(
+#       title="Hoods API",
+#       default_version='v1',
+#       description="API for neighbourhood website",
+#       #terms_of_service="https://www.google.com/policies/terms/",
+#       #contact=openapi.Contact(email="contact@snippets.local"),
+#       license=openapi.License(name="BSD License"),
+#    ),
+#    public=True,
+#    permission_classes=[permissions.AllowAny],
+# )
 
 urlpatterns = [
-    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    # re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    # re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    # path('', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     #endpoints: /swagger.json, #/swagger.yaml, #/swagger/, /redoc/
-    #path('', include('main.urls')),
+    path('', include('main.urls')),
     path('admin/', admin.site.urls),
     path('api/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
+    
+
+
+
+
+
+
 ]
 
